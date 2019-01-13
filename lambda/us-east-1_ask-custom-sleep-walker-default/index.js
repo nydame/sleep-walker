9e6dc8833b5a0dd87327ff681d5ddf1bb04e9bd3
CodeFreq <codefreq@Macintosh-256.local>
2019-01-12 16:22:41 -0800

    Simplify opening speech in response to user feedback.

--- lambda/us-east-1_ask-custom-sleep-walker-default/index.js
+++ lambda/us-east-1_ask-custom-sleep-walker-default/index.js
@@ -233,13 +233,13 @@ const FallbackHandler = {
   },
   handle(handlerInput) {
     const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    const speakOutput =
+      requestAttributes.t("FALLBACK_MESSAGE") +
+      requestAttributes.t("HELP_REPROMPT");
     return handlerInput.responseBuilder
-      .speak(requestAttributes.t("FALLBACK_MESSAGE"))
+      .speak(speakOutput)
       .reprompt(requestAttributes.t("FALLBACK_REPROMPT"))
-      .withSimpleCard(
-        requestAttributes.t("SKILL_NAME"),
-        requestAttributes.t("FALLBACK_MESSAGE")
-      )
+      .withSimpleCard(requestAttributes.t("SKILL_NAME"), speakOutput)
       .getResponse();
   }
 };
@@ -349,33 +349,7 @@ exports.handler = dashbot.handler(
     .lambda()
 );
 
-// translations
-// const deData = {
-//   translation: {
-//     SKILL_NAME: 'Weltraumwissen',
-//     GET_FACT_MESSAGE: 'Hier sind deine Fakten: ',
-//     HELP_MESSAGE: 'Du kannst sagen, „Nenne mir einen Fakt über den Weltraum“, oder du kannst „Beenden“ sagen... Wie kann ich dir helfen?',
-//     HELP_REPROMPT: 'Wie kann ich dir helfen?',
-//     FALLBACK_MESSAGE: 'Die Weltraumfakten Skill kann dir dabei nicht helfen. Sie kann dir Fakten über den Raum erzählen, wenn du dannach fragst.',
-//     FALLBACK_REPROMPT: 'Wie kann ich dir helfen?',
-//     ERROR_MESSAGE: 'Es ist ein Fehler aufgetreten.',
-//     STOP_MESSAGE: 'Auf Wiedersehen!',
-//     FACTS:
-//       [
-//         'Ein Jahr dauert auf dem Merkur nur 88 Tage.',
-//         'Die Venus ist zwar weiter von der Sonne entfernt, hat aber höhere Temperaturen als Merkur.',
-//         'Venus dreht sich entgegen dem Uhrzeigersinn, möglicherweise aufgrund eines früheren Zusammenstoßes mit einem Asteroiden.',
-//         'Auf dem Mars erscheint die Sonne nur halb so groß wie auf der Erde.',
-//         'Jupiter hat den kürzesten Tag aller Planeten.',
-//       ],
-//   },
-// };
-//
-// const dedeData = {
-//   translation: {
-//     SKILL_NAME: 'Weltraumwissen auf Deutsch',
-//   },
-// };
+// TODO: translations
 
 const enData = {
   translation: {
@@ -384,10 +358,10 @@ const enData = {
       "Hi! I'm Sleep Walker, and I can help you get ready for bed. ",
     GET_FACT_MESSAGE: "Did you know? ",
     HELP_MESSAGE:
-      "You can say 'tell sleep walker to start my routine' to start a bed time routine. Say 'tell sleep walker to continue' to go on to the next step or say 'repeat that' to hear the current step again. You can also say 'ask sleep walker for a sleep fact'. What would you like to do?",
+      "You can say 'start my routine' to start a bed time routine. Say 'tell sleep walker to continue' to go on to the next step. You can also say 'repeat that' or 'ask sleep walker for a sleep fact'. What would you like to do?",
     HELP_REPROMPT: "What can I help you with?",
     FALLBACK_MESSAGE:
-      "Sleep Walker can't help you with that.  It can help you follow a bedtime routine, or tell you cool facts about sleep. What can I help you with?",
+      "Sleep Walker  can help you follow a bedtime routine, or tell you cool facts about sleep.",
     FALLBACK_REPROMPT: "What can I help you with?",
     ERROR_MESSAGE: "Sorry, an error occurred.",
     STOP_MESSAGE: "Goodbye!",
@@ -425,7 +399,7 @@ const enData = {
       "Like a boss. "
     ],
     ROUTINE_STEP_START:
-      "The very first step is to simply stop working or eating. If you are enjoying a warm beverage, that's fine.",
+      "The very first step is to simply stop working or eating. If you are enjoying a warm, uncaffeinated beverage, that's fine.",
     ROUTINE_STEP_TWO:
       "Step 2. List up to 3 well-defined tasks that you want to accomplish tomorrow. You can use your phone or a piece or paper, or even just make a mental note.",
     ROUTINE_STEP_THREE:
@@ -475,122 +449,6 @@ const enusData = {
     SKILL_NAME: "American Sleep Walker"
   }
 };
-
-// const esData = {
-//   translation: {
-//     SKILL_NAME: "Curiosidades del Espacio",
-//     GET_FACT_MESSAGE: "Aquí está tu curiosidad: ",
-//     HELP_MESSAGE:
-//       "Puedes decir dime una curiosidad del espacio o puedes decir salir... Cómo te puedo ayudar?",
-//     HELP_REPROMPT: "Como te puedo ayudar?",
-//     FALLBACK_MESSAGE:
-//       "La skill Curiosidades del Espacio no te puede ayudar con eso.  Te puede ayudar a descubrir curiosidades sobre el espacio si dices dime una curiosidad del espacio. Como te puedo ayudar?",
-//     FALLBACK_REPROMPT: "Como te puedo ayudar?",
-//     ERROR_MESSAGE: "Lo sentimos, se ha producido un error.",
-//     STOP_MESSAGE: "Adiós!",
-//     FACTS: [
-//       "Un año en Mercurio es de solo 88 días",
-//       "A pesar de estar más lejos del Sol, Venus tiene temperaturas más altas que Mercurio",
-//       "En Marte el sol se ve la mitad de grande que en la Tierra",
-//       "Jupiter tiene el día más corto de todos los planetas",
-//       "El sol es una esféra casi perfecta"
-//     ]
-//   }
-// };
-//
-// const esesData = {
-//   translation: {
-//     SKILL_NAME: "Curiosidades del Espacio para España"
-//   }
-// };
-//
-// const esmxData = {
-//   translation: {
-//     SKILL_NAME: "Curiosidades del Espacio para México"
-//   }
-// };
-//
-// const frData = {
-//   translation: {
-//     SKILL_NAME: "Anecdotes de l'Espace",
-//     GET_FACT_MESSAGE: "Voici votre anecdote : ",
-//     HELP_MESSAGE:
-//       "Vous pouvez dire donne-moi une anecdote, ou, vous pouvez dire stop... Comment puis-je vous aider?",
-//     HELP_REPROMPT: "Comment puis-je vous aider?",
-//     FALLBACK_MESSAGE:
-//       "La skill des anecdotes de l'espace ne peux vous aider avec cela. Je peux vous aider à découvrir des anecdotes sur l'espace si vous dites par exemple, donne-moi une anecdote. Comment puis-je vous aider?",
-//     FALLBACK_REPROMPT: "Comment puis-je vous aider?",
-//     ERROR_MESSAGE: "Désolé, une erreur est survenue.",
-//     STOP_MESSAGE: "Au revoir!",
-//     FACTS: [
-//       "Une année sur Mercure ne dure que 88 jours.",
-//       "En dépit de son éloignement du Soleil, Vénus connaît des températures plus élevées que sur Mercure.",
-//       "Sur Mars, le Soleil apparaît environ deux fois plus petit que sur Terre.",
-//       "De toutes les planètes, Jupiter a le jour le plus court.",
-//       "Le Soleil est une sphère presque parfaite."
-//     ]
-//   }
-// };
-//
-// const frfrData = {
-//   translation: {
-//     SKILL_NAME: "Anecdotes françaises de l'espace"
-//   }
-// };
-//
-// const itData = {
-//   translation: {
-//     SKILL_NAME: "Aneddoti dallo spazio",
-//     GET_FACT_MESSAGE: "Ecco il tuo aneddoto: ",
-//     HELP_MESSAGE:
-//       'Puoi chiedermi un aneddoto dallo spazio o puoi chiudermi dicendo "esci"... Come posso aiutarti?',
-//     HELP_REPROMPT: "Come posso aiutarti?",
-//     FALLBACK_MESSAGE:
-//       "Non posso aiutarti con questo. Posso aiutarti a scoprire fatti e aneddoti sullo spazio, basta che mi chiedi di dirti un aneddoto. Come posso aiutarti?",
-//     FALLBACK_REPROMPT: "Come posso aiutarti?",
-//     ERROR_MESSAGE: "Spiacenti, si è verificato un errore.",
-//     STOP_MESSAGE: "A presto!",
-//     FACTS: [
-//       "Sul pianeta Mercurio, un anno dura solamente 88 giorni.",
-//       "Pur essendo più lontana dal Sole, Venere ha temperature più alte di Mercurio.",
-//       "Su Marte il sole appare grande la metà che su la terra. ",
-//       "Tra tutti i pianeti del sistema solare, la giornata più corta è su Giove.",
-//       "Il Sole è quasi una sfera perfetta."
-//     ]
-//   }
-// };
-//
-// const ititData = {
-//   translation: {
-//     SKILL_NAME: "Aneddoti dallo spazio"
-//   }
-// };
-//
-// const jpData = {
-//   translation: {
-//     SKILL_NAME: "日本語版豆知識",
-//     GET_FACT_MESSAGE: "知ってましたか？",
-//     HELP_MESSAGE:
-//       "豆知識を聞きたい時は「豆知識」と、終わりたい時は「おしまい」と言ってください。どうしますか？",
-//     HELP_REPROMPT: "どうしますか？",
-//     ERROR_MESSAGE: "申し訳ありませんが、エラーが発生しました",
-//     STOP_MESSAGE: "さようなら",
-//     FACTS: [
-//       "水星の一年はたった88日です。",
-//       "金星は水星と比べて太陽より遠くにありますが、気温は水星よりも高いです。",
-//       "金星は反時計回りに自転しています。過去に起こった隕石の衝突が原因と言われています。",
-//       "火星上から見ると、太陽の大きさは地球から見た場合の約半分に見えます。",
-//       '木星の<sub alias="いちにち">1日</sub>は全惑星の中で一番短いです。',
-//       "天の川銀河は約50億年後にアンドロメダ星雲と衝突します。"
-//     ]
-//   }
-// };
-//
-// const jpjpData = {
-//   translation: {
-//     SKILL_NAME: "日本語版豆知識"
-//   }
-// };
 
 // constructs i18n and l10n data structure
 // translations for this sample can be found at the end of this file
